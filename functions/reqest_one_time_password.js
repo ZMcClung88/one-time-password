4024f252aabe58e2b5d987563e633ea5ccfa3159
Zac McClung <zac.mcclung370@gmail.com>
2018-05-30 10:27:45 -0600

    generating access codes

--- functions/reqest_one_time_password.js
+++ functions/reqest_one_time_password.js
@@ -14,11 +14,25 @@ module.exports = function(req, res) {
     .then(userRecord => {
       const code = Math.floor(Math.random() * 8999 + 1000);
 
-      twilio.messages.create({
-        body: 'Your code is ' + code,
-        to: phone,
-        from: '+13176718965'
-      });
+      twilio.messages.create(
+        {
+          body: 'Your code is ' + code,
+          to: phone,
+          from: '+13176718965'
+        },
+        err => {
+          if (err) {
+            return res.status(422).send(err);
+          }
+
+          admin
+            .database()
+            .ref('users/' + phone)
+            .update({ code: code, codeValid: true }, () => {
+              res.send({ success: true });
+            });
+        }
+      );
     })
     .catch(err => {
       res.status(422).send({ error: err });
